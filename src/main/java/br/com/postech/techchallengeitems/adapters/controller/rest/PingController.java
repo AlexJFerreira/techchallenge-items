36b0696a8bf9946b69c8ad906fb60341b997f6b7
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-19 18:33:55 -0300

    adding ping controller to use in aws

---
+++ src/main/java/br/com/postech/techchallengeitems/adapters/controller/rest/PingController.java
@@ -0,0 +1,23 @@
+package br.com.postech.techchallengeitems.adapters.controller.rest;
+
+import br.com.postech.techchallengeitems.adapters.controller.rest.response.ItemSearchByTypeResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PingController {
+
+  @GetMapping("/")
+  @ResponseStatus(HttpStatus.OK)
+  public void ping() {
+
+  }
+
+}
